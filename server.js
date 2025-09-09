b44ed75c6a1ad64c613a44d9f73342c686b62dfb
LiorKirshner <liorkk7@gmail.com>
2025-09-09 14:17:43 +0300

    ex1 - create a /sanity route that simply responds

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const path = require("path");
-const app = express();
-
 const api = require("./server/routes/api");
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static(path.join(__dirname, "node_modules")));
 app.use("/", api);
+
+app.get("/sanity", function (req, res) {
+  res.send("server is up and running");
+});
 
 const port = 1337; //because why not
 app.listen(port, function () {
